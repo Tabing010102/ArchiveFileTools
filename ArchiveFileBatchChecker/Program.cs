6a54514944a420b2cd65be4dc84c122998df8065
Tabing010102 <tabing010102@outlook.com>
2023-11-21 14:31:01 +0800

    chore: vs init

---
+++ ArchiveFileBatchChecker/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ArchiveFileBatchChecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
