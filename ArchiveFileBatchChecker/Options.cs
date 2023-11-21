744f5c10d0642a29041e499496dad51cbb209a16
Tabing010102 <tabing010102@outlook.com>
2023-11-21 15:51:23 +0800

    feat: impl basic dir, extension, password

---
+++ ArchiveFileBatchChecker/Options.cs
@@ -0,0 +1,19 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArchiveFileBatchChecker
+{
+    public class Options
+    {
+        [Option('d', "dir", Required = true, HelpText = "The directory to scan.")]
+        public string DirectoryPath { get; set; } = string.Empty;
+        [Option('e', "ext", Separator = ',', Default = "zip,rar,7z,001", HelpText = "The file extension to scan for.")]
+        public IList<string> Extensions { get; set; } = new List<string>();
+        [Option('p', "pwd", Separator = ',', Default = "", HelpText = "The password to use for the archive files.")]
+        public IList<string> Passwords { get; set; } = new List<string>();
+    }
+}
--- ArchiveFileBatchChecker/Program.cs
+++ ArchiveFileBatchChecker/Program.cs
@@ -1,10 +1,52 @@
-﻿namespace ArchiveFileBatchChecker
+﻿using CommandLine;
+using SevenZip;
+using System.Text.RegularExpressions;
+
+namespace ArchiveFileBatchChecker
 {
     internal class Program
     {
+        private static Regex _regex = new(@"\.part(?!0*1)\d*\.");
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Parser.Default.ParseArguments<Options>(args)
+                          .WithParsed<Options>(Run);
+        }
+
+        static void Run(Options options)
+        {
+            var dir = options.DirectoryPath;
+            var pwds = new List<string> { string.Empty };
+            pwds.AddRange(options.Passwords);
+            var files = new List<string>();
+            foreach (var ext in options.Extensions)
+            {
+                files.AddRange(Directory.EnumerateFiles(dir, $"*.{ext}", SearchOption.AllDirectories));
+            }
+            files = files.Where(x => _regex.IsMatch(x) == false).ToList();
+            foreach (var file in files)
+            {
+                var result = false;
+                foreach (var pwd in pwds)
+                {
+                    SevenZipExtractor extractor;
+                    if (pwd == string.Empty)
+                    {
+                        extractor = new SevenZipExtractor(file);
+                    }
+                    else
+                    {
+                        extractor = new SevenZipExtractor(file, pwd);
+                    }
+                    if (extractor.Check())
+                    {
+                        result = true;
+                        break;
+                    }
+                }
+                Console.WriteLine($"{file} check {result.ToString()}");
+            }
         }
     }
 }
